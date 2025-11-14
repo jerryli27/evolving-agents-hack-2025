5ac7627dc9820bc2b313f7c29e32a564b0bcdbfe
Yi <ywang485@gmail.com>
2025-11-14 14:53:43 -0800

    Prompt tweaking for more diverse rating

--- readers_parameters.py
+++ readers_parameters.py
@@ -5,9 +5,9 @@ agent_bank_path = "genagents/agent_bank/populations/gss_agents/"
 num_readers = 10
 novelty_weight = 0.3
 relevance_weight = 0.3
+quality_weight = 1.0 - novelty_weight - relevance_weight
 expectation_rubric = [
     "How similar do you find this story to the stories in the books you have read?",
-    "Based on all the books you have read before, to what extent can you predict the plot progression in this story?",
     "Based on all the books you have read before, how steoretypical do you find the characters in the story?",
     "Based on all the books you have read before, how expected do you find the ending of the story?"
 ]
@@ -16,8 +16,8 @@ relevance_rubric = [
     "how much do you resonate with this book you just read based on your personal life experience?"
 ]
 quality_rubric = [
-    "how cohenrent do you think the plot is?",
-    "how consistent do you think the characters are?"
+    "how coherent do you think the plot is? Be critical.",
+    "how consistent do you think the characters are? Be critical."
 ]
 open_ended_feedback = [
     "based on your background and personal experience, what do you like about this story?",
@@ -26,5 +26,5 @@ open_ended_feedback = [
 
 # Prompts 
 reading_decision_prompt = """
-You see a fiction titled {title}, with the following summary: {summary}.\n Would you be interested in reading this book?
+You see a fiction titled {title}, with the following summary: {summary}.\n Would you be interested in reading this book? Note that you are busy and you have limited time and attention. Only books that really stand out will be worth your time.
 """
